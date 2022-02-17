ee31211eb5c55cb9d39fe58be96ba574005ab68d
JaroslavOndrus <jaroslavondrus04@gmail.com>
2022-02-17 12:05:44 +0100

    remake join page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, basicRoutingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DiscoverPageComponent } from './discover-page/discover-page.component';
-import { JoinPageComponent } from './join-page/join-page.component';
-import { LogInPageComponent } from './log-in-page/log-in-page.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DiscoverPageComponent,
-    JoinPageComponent,
-    LogInPageComponent,
-    LandingPageComponent
+    basicRoutingComponents
   ],
   imports: [
     BrowserModule,
