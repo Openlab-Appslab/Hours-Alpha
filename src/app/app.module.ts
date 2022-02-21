ddf962b542b95c73c6ddce71eaa80a6d9a6df7f5
TimotejkoPantak <timopantakgit@gmail.com>
2022-02-21 11:46:24 +0100

    Add UserRole and AdminRole component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule, basicRoutingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminRoleComponent } from './admin-role/admin-role.component';
+import { UserRoleComponent } from './user-role/user-role.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    basicRoutingComponents
+    basicRoutingComponents,
+    AdminRoleComponent,
+    UserRoleComponent
   ],
   imports: [
     BrowserModule,
