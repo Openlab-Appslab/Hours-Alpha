2612c07b8c9d053f982158ec53091f167be96ae2
JaroslavOndrus <jaroslavondrus04@gmail.com>
2022-04-10 12:42:09 +0100

    design contact page, calculator page, info page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatInputModule } from '@angular/material/input';
-
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -31,6 +31,7 @@ import { MatInputModule } from '@angular/material/input';
     MatDividerModule,
     ReactiveFormsModule,
     MatInputModule,
+    MatSelectModule,
 
   ],
   providers: [],
